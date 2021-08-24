8d0392557f25c7cc9555d97f3dc779fcbdc0d038
Javicode9449 <javierhuttruiz@gmail.com>
2021-08-24 11:33:00 -0700

    first commit

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Randy' },
+    { id: 13, name: 'Kronos' },
+    { id: 14, name: 'Zues' },
+    { id: 15, name: 'Hercules' },
+    { id: 16, name: 'Hermes' },
+    { id: 17, name: 'Achilles' },
+    { id: 18, name: 'Agathor' },
+    { id: 19, name: 'Dr Doom' },
+    { id: 20, name: 'Dr Sad' }
+];
